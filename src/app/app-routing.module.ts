bc631865fc61bd844ba463d02d93ce96125d467b
Sam Schultz <sam.schultz@outlook.com>
2022-10-31 10:52:01 -0400

    InitCompletedStep10ofBinding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './misc/home/home.component';
+import { AboutComponent } from './misc/about/about.component';
+import { E404Component } from './misc/e404/e404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch:"full"},  //Always your first route
+
+  {path:"home", component: HomeComponent}, 
+  {path:"about", component: AboutComponent},
+
+  {path:"**", component: E404Component}   //Always your last route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
